8f47b76a72e3a2c174a9f0ad0f25fba208aa9ce8
HoldenWang <wagnhagnlll@163.com>
2017-03-15 07:46:49 +0800

    rm pyc

--- web_app/config.py
+++ web_app/config.py
@@ -1,9 +1,15 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
 	SQLALCHEMY_COMMIT_ON_TEARDOWN = True 
+	MAIL_SERVER = 'smtp.163.com'
+	MAIL_PORT = 25
+	MAIL_USE_TLS = True
+	MAIL_USERNAME = os.environ.get(MAIL_USERNAME)
+	MAIL_PASSWORD = os.environ.get(MAIL_PASSWORD)
 	FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
 	FLASKY_MAIL_SENDER = 'Flasky Admin <wagnhagnlll@163.com>'
 	FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
@@ -14,11 +20,11 @@ class Config:
 
 class DevelopmentConfig(Config):
 	DEBUG = True
-	MAIL_SERVER = 'smtp.163.com'
-	MAIL_PORT = 25
-	MAIL_USE_TLS = True
-	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+#	MAIL_SERVER = 'smtp.163.com'
+#	MAIL_PORT = 25
+#	MAIL_USE_TLS = True
+#	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+#	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
 		'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
