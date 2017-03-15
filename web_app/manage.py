400fb12ec8313de0e793c87487a2644b5cb7f09d
HoldenWang <wagnhagnlll@163.com>
2017-03-15 08:01:32 +0800

    add ignore & check in home

--- web_app/manage.py
+++ web_app/manage.py
@@ -9,17 +9,19 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app)
 
+
+def make_shell_context():
+	return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+
 @manager.command
 def test():
 	"""run the unit tests"""
 	import unittest
 	tests = unittest.TestLoader().discover('tests')
 	unittest.TextTesteRunner(verbosity=2).run(tests)
-
-def make_shell_context():
-	return dict(app=app, db=db, User=User, Role=Role)
-manager.add_command("shell", Shell(make_context=make_shell_context))
-manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
 	manager.run()
